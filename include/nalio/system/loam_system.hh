879b68bb5b8eb4730eda1da6410dc9dd019e3324
caoming <caoming@momenta.ai>
2022-06-06 19:56:50 +0800

    add associate in LOAM_system.

--- include/nalio/system/loam_system.hh
+++ include/nalio/system/loam_system.hh
@@ -7,7 +7,10 @@
 
 #include <condition_variable>
 #include <queue>
+#include <shared_mutex>
 #include <thread>
+
+#include <Eigen/Geometry>
 
 #include "nalio/data/message.hh"
 #include "nalio/factory/factory.hh"
@@ -24,6 +27,7 @@ class LOAMSystem final : public System {
  public:
   using Ptr = std::unique_ptr<LOAMSystem>;
   using PointCloudT = pcl::PointCloud<NalioPoint>;
+  using PointT = NalioPoint;
 
   LOAMSystem();
   ~LOAMSystem();
@@ -33,8 +37,21 @@ class LOAMSystem final : public System {
   void feedData(const MessagePackage& data);
 
  private:
+  struct EdgePair {
+    PointT ori_pt;
+    PointT neigh_pt[2];
+  };
+
+  struct PlanePair {
+    PointT ori_pt;
+    PointT neigh_pt[3];
+  };
   void propagate() override;
   void update() override;
+  void associate(const LOAMFeaturePackage::Ptr& prev_feature,
+                 const LOAMFeaturePackage::Ptr& curr_feature,
+                 std::vector<EdgePair>* edge_pairs,
+                 std::vector<PlanePair>* plane_pairs);
 
   Eigen::Isometry3d getEstimated() override;
 
@@ -43,14 +60,20 @@ class LOAMSystem final : public System {
 #ifdef USE_UNOS
   unos::Manifold state_;
 #else
-  
+  // x, y, z, qx, qy, qz, qw
+  double last_to_curr_[7];
 #endif
-  
+
   bool initialized_, running_;
   std::queue<LOAMFeaturePackage::Ptr> feature_package_list_;
   std::condition_variable feature_package_list_cv_;
   std::mutex feature_package_list_mutex_;
+  std::shared_mutex state_mutex_;
   std::thread update_thread_;
+  Eigen::Isometry3d last_state_transform_;
+  Eigen::Isometry3d curr_state_transform_;
+
+  const double kNearbyScan = 2.5;
 
 #ifdef NALIO_DEBUG
   ros::NodeHandle nh_;
@@ -58,6 +81,7 @@ class LOAMSystem final : public System {
   ros::Publisher less_sharp_feature_pub_;
   ros::Publisher flat_feature_pub_;
   ros::Publisher less_flat_feature_pub_;
+  ros::Publisher associate_pub_;
 #endif
 };
 
--- src/system/loam_system.cc
+++ src/system/loam_system.cc
@@ -4,14 +4,22 @@
 
 #ifdef NALIO_DEBUG
 #include <pcl/console/time.h>
+#include <pcl/kdtree/kdtree_flann.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <visualization_msgs/Marker.h>
 #endif
 
 namespace nalio {
 
 LOAMSystem::LOAMSystem()
-    : state_(unos::SO3(), unos::Vec3()), initialized_(false), running_(true) {}
+    :
+#ifdef USE_UNOS
+      state_(unos::SO3(), unos::Vec3()),
+#endif
+      initialized_(false),
+      running_(true) {
+}
 
 LOAMSystem::~LOAMSystem() {
   running_ = false;
@@ -31,7 +39,19 @@ void LOAMSystem::init() {
       nh_.advertise<sensor_msgs::PointCloud2>("NALIO/flat_feature", 1);
   less_flat_feature_pub_ =
       nh_.advertise<sensor_msgs::PointCloud2>("NALIO/less_flat_feature", 1);
+  associate_pub_ =
+      nh_.advertise<visualization_msgs::Marker>("NALIO/associate", 1);
 #endif
+
+#ifdef USE_UNOS
+  throw(std::logic_error("not implement yet."));
+#else
+  for (size_t i = 0; i < 7; ++i) {
+    last_to_curr_[i] = 0;
+  }
+  last_to_curr_[6] = 1;
+#endif
+
   update_thread_ = std::thread(&LOAMSystem::update, this);
   initialized_ = true;
 }
@@ -98,18 +118,156 @@ void LOAMSystem::propagate() {
 }
 
 void LOAMSystem::update() {
+  static LOAMFeaturePackage::Ptr prev_feature_package;
   while (running_) {
     std::unique_lock<std::mutex> lock(feature_package_list_mutex_);
     while (feature_package_list_.empty() && running_) {
       feature_package_list_cv_.wait(lock);
     }
     ROS_INFO_STREAM_FUNC("enter update.");
+    auto& curr_feature_package = feature_package_list_.front();
+    if (prev_feature_package) {
+      std::vector<EdgePair> edge_pairs;
+      std::vector<PlanePair> plane_pairs;
+      associate(prev_feature_package, curr_feature_package, &edge_pairs,
+                &plane_pairs);
+    }
+    prev_feature_package = curr_feature_package;
     feature_package_list_.pop();
   }
 }
 
+void LOAMSystem::associate(const LOAMFeaturePackage::Ptr& prev_feature,
+                           const LOAMFeaturePackage::Ptr& curr_feature,
+                           std::vector<EdgePair>* edge_pairs,
+                           std::vector<PlanePair>* plane_pairs) {
+  //* Edge feature association.
+  static pcl::KdTreeFLANN<PointT> edge_kdtree;
+  edge_kdtree.setInputCloud(prev_feature->less_sharp_cloud);
+  std::vector<int> pt_search_inds;
+  std::vector<float> pt_search_dists;
+  for (size_t ei = 0; ei < curr_feature->sharp_cloud->size(); ++ei) {
+    const PointT& pt_sel = curr_feature->sharp_cloud->at(ei);
+
+    edge_kdtree.nearestKSearch(pt_sel, 1, pt_search_inds, pt_search_dists);
+    const PointT& closest_pt =
+        (*prev_feature->less_sharp_cloud)[pt_search_inds[0]];
+    int second_closest_pt_ind = -1;
+    double second_closest_pt_dist = std::numeric_limits<double>::max();
+    for (size_t ej = pt_search_inds[0] + 1;
+         ej < prev_feature->less_sharp_cloud->size(); ++ej) {
+      if ((*prev_feature->less_sharp_cloud)[ej].line <= closest_pt.line) {
+        continue;
+      }
+
+      if ((*prev_feature->less_sharp_cloud)[ej].line >
+          closest_pt.line + kNearbyScan) {
+        break;
+      }
+
+      double distance =
+          ((*prev_feature->less_sharp_cloud)[ej].getVector3fMap() -
+           pt_sel.getVector3fMap())
+              .squaredNorm();
+      if (distance < second_closest_pt_dist) {
+        second_closest_pt_ind = ej;
+        second_closest_pt_dist = distance;
+      }
+    }
+
+    for (size_t ej = pt_search_inds[0] - 1; ej > 0; --ej) {
+      if ((*prev_feature->less_sharp_cloud)[ej].line >= closest_pt.line) {
+        continue;
+      }
+
+      if ((*prev_feature->less_sharp_cloud)[ej].line <
+          closest_pt.line - kNearbyScan) {
+        break;
+      }
+      double distance =
+          ((*prev_feature->less_sharp_cloud)[ej].getVector3fMap() -
+           pt_sel.getVector3fMap())
+              .squaredNorm();
+      if (distance < second_closest_pt_dist) {
+        second_closest_pt_ind = ej;
+        second_closest_pt_dist = distance;
+      }
+    }
+
+    if (second_closest_pt_ind > 0) {
+      EdgePair edge_pair;
+      edge_pair.ori_pt = pt_sel;
+      edge_pair.neigh_pt[0] = closest_pt;
+      edge_pair.neigh_pt[1] =
+          (*prev_feature->less_sharp_cloud)[second_closest_pt_ind];
+      edge_pairs->push_back(std::move(edge_pair));
+    }
+  }
+
+  static pcl::KdTreeFLANN<PointT, PointT> plane_kdtree;
+  plane_kdtree.setInputCloud(prev_feature->less_flat_cloud);
+  for (size_t pi = 0; pi < curr_feature->flat_cloud->size(); ++pi) {
+    const PointT& pt_sel = curr_feature->flat_cloud->at(pi);
+    plane_kdtree.nearestKSearch(pt_sel, 1, pt_search_inds, pt_search_dists);
+    const PointT& closest_pt =
+        (*prev_feature->less_flat_cloud)[pt_search_inds[0]];
+    int l_ind = -1, m_ind = -1;
+    double l_distance = std::numeric_limits<double>::max();
+    double m_distance = std::numeric_limits<double>::max();
+
+    for (size_t pj = pt_search_inds[0] + 1;
+         pj < prev_feature->less_flat_cloud->size(); ++pj) {
+      if ((*prev_feature->less_flat_cloud)[pj].line <= closest_pt.line) {
+        continue;
+      }
+      if ((*prev_feature->less_flat_cloud)[pj].line >
+          closest_pt.line + kNearbyScan) {
+        break;
+      }
+      double distance = ((*prev_feature->less_flat_cloud)[pj].getVector3fMap() -
+                         closest_pt.getVector3fMap())
+                            .squaredNorm();
+      if (distance < l_distance) {
+        l_ind = pj;
+        l_distance = distance;
+      }
+    }
+
+    for (size_t pj = pt_search_inds[0] - 1; pj > 0; --pj) {
+      if ((*prev_feature->less_flat_cloud)[pj].line >= closest_pt.line) {
+        continue;
+      }
+      if ((*prev_feature->less_flat_cloud)[pj].line <
+          closest_pt.line - kNearbyScan) {
+        break;
+      }
+      double distance = ((*prev_feature->less_flat_cloud)[pj].getVector3fMap() -
+                         closest_pt.getVector3fMap())
+                            .squaredNorm();
+      if (distance < m_distance) {
+        m_ind = pj;
+        m_distance = distance;
+      }
+    }
+
+    if (l_ind >= 0 && m_ind >= 0) {
+      PlanePair plane_pair;
+      plane_pair.ori_pt = pt_sel;
+      plane_pair.neigh_pt[0] = closest_pt;
+      plane_pair.neigh_pt[1] = (*prev_feature->less_flat_cloud)[l_ind];
+      plane_pair.neigh_pt[2] = (*prev_feature->less_flat_cloud)[m_ind];
+      plane_pairs->push_back(std::move(plane_pair));
+    }
+  }
+
+#ifdef NALIO_DEBUG
+
+#endif
+}
+
 Eigen::Isometry3d LOAMSystem::getEstimated() {
-  // return stateToEigen(state_.get());
+  Eigen::Isometry3d ret;
+  return curr_state_transform_;
 }
 
 }  // namespace nalio
